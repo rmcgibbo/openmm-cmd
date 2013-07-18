dec0508719c457db5f90cc134a77375c462e75eb
Robert McGibbon <rmcgibbo@gmail.com>
2013-07-18 12:20:27 -0700

    add pretty formatting of remaining time (h:m:s) to progress reporter

--- ipcfg/progressreporter.py
+++ ipcfg/progressreporter.py
@@ -2,6 +2,7 @@
 # Imports
 #-----------------------------------------------------------------------------
 # stdlib
+import math
 import time
 
 # openmm
@@ -24,7 +25,7 @@ class ProgressReporter(StateDataReporter):
         if simulation.topology.getUnitCellDimensions() is not None:
             self._volume = True
             self._density = True
-        
+
         # this needs to come after _density and _volume are set so
         # that the mass gets computed, if needed.
         super(ProgressReporter, self)._initializeConstants(simulation)
@@ -41,20 +42,21 @@ class ProgressReporter(StateDataReporter):
         progressPercent = 100 * float(simulation.currentStep - self._initialStep) / self._totalSteps
         if progressPercent > 0:
             timeLeft = (time.time() - self._initialWallTime) * (100.0 - progressPercent) / progressPercent
-            
+
             elapsedSim = (state.getTime() - self._initialSimTime).value_in_unit(unit.nanoseconds)
             walltime = ((time.time() - self._initialWallTime)*unit.seconds).value_in_unit(unit.days)
-            rate = elapsedSim / walltime                   
+            rate = elapsedSim / walltime
         else:
-            timeLeft = '??'
+            timeLeft = float('nan')
             rate = 0
-        
-        values = [progressPercent, timeLeft, rate] + super(ProgressReporter, self)._constructReportValues(simulation, state)
+
+        values = [progressPercent, self.pretty_time(timeLeft), rate] + \
+                 super(ProgressReporter, self)._constructReportValues(simulation, state)
         return values
-        
+
     def _constructHeaders(self):
         headers = [('Progress', '(%)'),
-                   ('E.T.A', '(s)'),
+                   ('WallTime Left', '(d:h:m:s)'),
                    ('Speed', '(ns/day)'),
                    ('Time',  '(ps)'),
                    ('P.E.', '(kJ/mol)'),
@@ -62,8 +64,8 @@ class ProgressReporter(StateDataReporter):
                    ('Total E.', '(kJ/mol)'),
                    ('Temp', '(K)'),
                   ]
-        
-        widths =  [8,          13,      8,        13,       15, 
+
+        widths =  [8,          13,      8,        13,       15,
                    13,         13,      13,       8]
         formats = ['%7.3f%%', '%13.5s', '%8.2f', '%13.5f', '%15.5f',
                    '%13.5f', '%13.5f', '%13.5f', '%8.2f']
@@ -76,21 +78,21 @@ class ProgressReporter(StateDataReporter):
             headers.append(('Rho', '(g/mL)'))
             formats.append('%10.4f')
             widths.append(10)
-            
+
         self._formats = formats
-        
+
         row1, row2 = zip(*headers)
         headerwidths = ['%{w}s'.format(w=w) for w in widths]
         print >>self._out, ' '.join(f % e for f, e in zip(headerwidths, row1))
         print >>self._out, ' '.join(f % e for f, e in zip(headerwidths, row2))
 
-    
+
     def report(self, simulation, state):
         if not self._hasInitialized:
             self._initializeConstants(simulation, state)
             self._constructHeaders()
             self._hasInitialized = True
-        
+
         # Check for errors.
         self._checkForErrors(simulation, state)
 
@@ -98,3 +100,30 @@ class ProgressReporter(StateDataReporter):
         values = self._constructReportValues(simulation, state)
 
         print >>self._out, ' '.join(f % v for f, v in zip(self._formats, values))
+
+    def pretty_time(self, secs):
+        """Format the time in a pretty way"""
+
+        if math.isnan(secs):
+            return "??"
+        secs = int(secs)
+
+        days = secs // 86400
+        secs -= 86400*days
+
+        hrs = secs // 3600
+        secs -= 3600*hrs
+
+        mins = secs // 60
+        secs -= 60*mins
+
+        if days > 0:
+            s = "%d:%d:%02d:%02d" % (days, hours, mins, secs)
+        if hrs > 0:
+            s = "%d:%02d:%02d" % (hours, mins, secs)
+        elif mins > 0:
+            s = "%d:%02d" % (mins, secs)
+        else:
+            s = "0:%02d" % secs
+
+        return s
